f77977154b1ca7af0d5ae771dbd78099e679277d
Plandevida <plandevida@planes.com>
2015-03-12 14:06:34 +0100

    footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Web AW' });
+	res.render('index', { title: 'Web AW' });
+});
+
+router.get('/leoshop', function(req, res, next) {
+	res.render('leoshop-web/index');
+});
+
+router.get('/shop', function(req, res, next) {
+	res.render('shop/index');
 });
 
 module.exports = router;
